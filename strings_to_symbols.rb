34f44b4b628d6833bf901f8f97489cdeef0c643d
Val Grigoriou <valgrigoriou@gmail.com>
2016-01-08 01:01:42 -0500

    add strings to symbol conversion file

---
+++ strings_to_symbols.rb
@@ -0,0 +1,17 @@
+my_own_menu = { 'tator_tots' => 2, 
+'fancy_toast' => 3, 
+'omelette' => 4, 
+'tiny_burger' => 4, 
+'chunky_bacon' => 1,
+'root_beer_float' => 2,
+'egg_nog' => 2
+}
+puts "Object ID before: #{my_own_menu.object_id}"
+
+my_own_menu.keys.each do |key|
+	my_own_menu[key.to_sym] = my_own_menu.delete(key)
+end
+
+puts "Object ID after: #{my_own_menu.object_id}"
+
+puts my_own_menu
